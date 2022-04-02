8368f1f4bb1c2db7f8a3f6ec727709c54bf98bf8
jw10126121 <10126121@qq.com>
2022-04-02 21:10:38 +0800

    no message

--- lib/fastlane/plugin/appinfo/version.rb
+++
@@ -1,5 +0,0 @@
-module Fastlane
-  module Appinfo
-    VERSION = "0.1.0"
-  end
-end
--- spec/appinfo_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::AppinfoAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The appinfo plugin is working!")
-
-      Fastlane::Actions::AppinfoAction.run(nil)
-    end
-  end
-end
